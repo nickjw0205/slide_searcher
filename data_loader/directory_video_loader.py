9632455695bf85edc459749a4bae80e44c079ab2
Lee-Null <nullist.null@gmail.com>
2021-07-21 15:06:01 +0900

    feat: impl video_loader and slide_searcher

--- data_loader/directory_video_loader.py
+++ data_loader/directory_video_loader.py
@@ -1,5 +1,21 @@
+import cv2
+
 from domain.video_loader import VideoLoader
 
 class DirectoryVideoLoader(VideoLoader):
-    def getVideo():
-        pass
+    def __init__(self, path):
+        self.video = cv2.VideoCapture(path)
+        self.fps = self.video.get(cv2.CAP_PROP_FPS)
+
+    def frame_time(self, count):
+        return count / self.fps
+
+    def frames(self):
+        frame_count = 0
+        while(self.video.isOpened()):
+            _, frame = self.video.read()
+            if (type(frame) == type(None)):
+                break
+            frame_count += 1
+            yield self.frame_time(frame_count), frame
+        self.video.release()
