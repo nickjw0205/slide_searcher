845d6aa5face213dc707a7da661d7e6eee77a668
nickjw0205 <nomadnick02@gmail.com>
2021-07-21 15:41:19 +0900

    add code

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from data_loader.ppt_image_loader import PPTImageLoader
 from data_loader.directory_video_loader import DirectoryVideoLoader
 from domain.slide_searcher import SlideSearcher
+import argparse
 
-def extract_args():
-    pass
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--video", dest="video", required=True,
+                    help="the path to your video file to be analyzed")
+parser.add_argument("-p", "--ppt", dest="ppt", required=True,
+                    help="the path to your ppt file to be analyzed")
+args = vars(parser.parse_args())
 
 def main():
-    ppt_path, video_path = extract_args()
+    ppt_path, video_path = args.video, args.ppt
 
     video_loader = DirectoryVideoLoader(video_path)
     image_loader = PPTImageLoader(ppt_path)
